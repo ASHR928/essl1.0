2d1ec6e81bd608431bfa44d991b4cc881f5ec0a7
KIRAN RAVINDRA DIVATE <divate26@gmail.com>
2024-08-12 15:12:34 +0530

    update

--- essl-frontend/src/app/_Resolver/canlendar.resolver.ts
+++
@@ -1,7 +0,0 @@
-import { inject } from '@angular/core';
-import { ResolveFn } from '@angular/router';
-import { CanlendarService } from './canlendar.service';
-
-export const canlendarResolver: ResolveFn<boolean> = (route, state) => {
-  return inject(CanlendarService).calendar;
-};
