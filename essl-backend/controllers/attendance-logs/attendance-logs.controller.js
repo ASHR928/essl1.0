a2b9c3a710f228e2ef3c1ff092f98733dd55fa91
unknown <gunjane3026@gmail.com>
2024-08-21 22:12:02 +0530

    update

--- essl-backend/controllers/attendance-logs/attendance-logs.controller.js
+++ essl-backend/controllers/attendance-logs/attendance-logs.controller.js
@@ -1,4 +1,5 @@
 const AttendanceLog = require('../attendance-logs/attendance-logs');
+const sequelize = require('../../sequelizeconn')
 
 // Get all attendance logs
 exports.getAllAttendanceLogs = async (req, res) => {
@@ -29,6 +30,39 @@ exports.getAttendanceLogById = async (req, res) => {
     res.status(500).json({ message: error.message });
   }
 };
+
+// Get a specific attendance log joining two tables
+exports.getAttendanceLogByEmployeeCode = async (req, res) => {
+  try {
+
+    console.log(req.params);
+    const { empId } = req.params;
+    console.log(empId);
+
+    const query = `SELECT a.Status, a.AttendanceDate, emp.EmployeeCode
+            FROM AttendanceLogs as a
+            INNER JOIN employees as emp
+          ON a.EmployeeId = emp.EmployeeId where emp.EmployeeCode = :empId;`
+
+    console.log(query);
+
+    const attendanceLog = await sequelize.query(query, {
+      replacements: { empId },
+      type: Sequelize.QueryTypes.SELECT,
+    });
+    console.log(attendanceLog);
+
+    if (attendanceLog) {
+      res.json(attendanceLog);
+    } else {
+      res.status(404).json({ message: 'AttendanceLog not found' });
+    }
+  } catch (error) {
+    res.status(500).json({ message: error.message });
+  }
+};
+
+
 
 // Create a new attendance log
 exports.createAttendanceLog = async (req, res) => {
