f2f7bbed5ebcf26748e94d39134f2a3b6b0c8de7
KIRAN RAVINDRA DIVATE <divate26@gmail.com>
2024-08-23 22:48:49 +0530

    update attendance punch logs

--- essl-backend/controllers/attendance-punch-logs/attendance-punch-logs.controller.js
+++ essl-backend/controllers/attendance-punch-logs/attendance-punch-logs.controller.js
@@ -4,8 +4,6 @@ var Sequelize = require("sequelize");
 
 exports.getAttendancePunchLog = async (req, res) => {
   try {
-    const { empId } = req.params;
-
     const query = `select emp.Emp_ID, emp.Emp_Name, emp.Emp_Alias_Name, emp.Emp_Designation, emp.Emp_Team_Name, emp.Emp_Department_Name, ptd.date, ptd.hh_mm
 from EMPLOYEE_MASTER as emp inner join PunchTimeDetails as ptd on ptd.tktno = emp.Emp_ID;`;
 
