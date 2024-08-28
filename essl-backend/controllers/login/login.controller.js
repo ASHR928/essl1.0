9821ec487b4d9a9e43e88f88eb7e1b579aab1fe2
KIRAN RAVINDRA DIVATE <divate26@gmail.com>
2024-08-28 16:22:47 +0530

    update

--- essl-backend/controllers/login/login.controller.js
+++ essl-backend/controllers/login/login.controller.js
@@ -1,35 +1,55 @@
-const mssql = require('mssql');
+const mssql = require("mssql");
+const encrypt = require("../../EncryptDecryptPwd/encrypt");
 
 exports.Login = async (req, res) => {
-    try {
-        data = req.body;
-        const que = `SELECT Emp_ID, Role_ID, Email FROM USER_MASTER WHERE Email='${data.email}' and Password='${data.pwd}' and Role_ID=${data.Role}`;
+  try {
+    data = req.body;
+    await encrypt.encrypt(data.pwd).then((response) => {
+      const query = `SELECT Password FROM USER_MASTER WHERE Email='${data.email}' and Role_ID=${data.Role}`;
 
-        await mssql.query(que).then(result => {
-            if (result.recordset.length <= 0) {
-                res.json({ sqlMessage: 'User name or password is incorrect' });
-            } else {
-                res.send(result.recordset);
-            }
-        });
-    } catch (error) {
-        res.status(500).json({ sqlMessage: 'Internal Server Error ' + error });
-    }
-}
+      mssql.query(query).then((result) => {
+        if (result.recordset.length <= 0) {
+          res.json({
+            sqlMessage: "User name or password is incorrect",
+          });
+        } else {
+          encrypt
+            .verify(data.pwd, result.recordset[0].Password)
+            .then((verifyPwd) => {
+                console.log(verifyPwd);
+              if (verifyPwd) {
+                const que = `SELECT Emp_ID, Role_ID, Email FROM USER_MASTER WHERE Email='${data.email}' and Password='${result.recordset[0].Password}' and Role_ID=${data.Role}`;
+
+                mssql.query(que).then((result) => {
+                  res.send(result.recordset);
+                });
+              } else {
+                res.json({
+                  sqlMessage: "User name or password is incorrect",
+                });
+              }
+            });
+        }
+      });
+    });
+  } catch (error) {
+    res.status(500).json({ sqlMessage: "Internal Server Error " + error });
+  }
+};
 
 exports.insertUsersDetails = async (req, res) => {
-    try {
-        const { Emp_ID, Password, Role_ID, Updated_By_UserID } = req.body;
+  try {
+    const { Emp_ID, Password, Role_ID, Updated_By_UserID } = req.body;
 
-        const newUser = await UserMaster.create({
-            Emp_ID,
-            Password,
-            Role_ID,
-            Updated_By_UserID
-        });
+    const newUser = await UserMaster.create({
+      Emp_ID,
+      Password,
+      Role_ID,
+      Updated_By_UserID,
+    });
 
-        res.status(201).json(newUser);
-    } catch (error) {
-        res.status(500).json({ sqlMessage: 'Internal Server Error ' + error });
-    }
-}
+    res.status(201).json(newUser);
+  } catch (error) {
+    res.status(500).json({ sqlMessage: "Internal Server Error " + error });
+  }
+};
