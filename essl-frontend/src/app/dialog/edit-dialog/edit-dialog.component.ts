a618edce59e8464ab8cd741e58ec673e5ca6bee2
KIRAN RAVINDRA DIVATE <divate26@gmail.com>
2024-10-02 14:54:19 +0530

    update

--- essl-frontend/src/app/dialog/edit-dialog/edit-dialog.component.ts
+++ essl-frontend/src/app/dialog/edit-dialog/edit-dialog.component.ts
@@ -2,10 +2,9 @@ import { Component, Inject } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { MaterialModule } from '../../_Material/material/material.module';
 import { MAT_DIALOG_DATA, MatDialogRef } from '@angular/material/dialog';
-import { MatNativeDateModule } from '@angular/material/core';
-import { MatDatepickerModule } from '@angular/material/datepicker';
 import { HttpModule } from '../../_Http/http/http.module';
 import { CommonService } from '../../_Resolver/common.service';
+import { JsonPipe } from '@angular/common';
 
 @Component({
   selector: 'app-edit-dialog',
@@ -13,17 +12,91 @@ import { CommonService } from '../../_Resolver/common.service';
   imports: [
     FormsModule,
     MaterialModule,
-    HttpModule
+    HttpModule,
+    JsonPipe
   ],
   templateUrl: './edit-dialog.component.html',
   styleUrl: './edit-dialog.component.scss'
 })
 export class EditDialogComponent {
+  desig = [
+    { SrNo: 1, Designation: "Executive Director" },
+    { SrNo: 2, Designation: "CFO" },
+    { SrNo: 3, Designation: "Director" },
+    { SrNo: 4, Designation: "Operations Manager" },
+    { SrNo: 5, Designation: "Sales Manager" },
+    { SrNo: 6, Designation: "HR Manager" },
+    { SrNo: 7, Designation: "Backend Manager" },
+    { SrNo: 8, Designation: "CS Manager" },
+    { SrNo: 9, Designation: "Product Manager" },
+    { SrNo: 10, Designation: "Quality Manager" },
+    { SrNo: 11, Designation: "BDM" },
+    { SrNo: 12, Designation: "ABDM" },
+    { SrNo: 13, Designation: "Admin Manager" },
+    { SrNo: 14, Designation: "Assistant Manager" },
+    { SrNo: 15, Designation: "Accountant" },
+    { SrNo: 16, Designation: "Assistant Accountant" },
+    { SrNo: 17, Designation: "Team Leader" },
+    { SrNo: 18, Designation: "HR Executive" },
+    { SrNo: 19, Designation: "Finance Executive" },
+    { SrNo: 20, Designation: "WFM Executive" },
+    { SrNo: 21, Designation: "Quality Executive" },
+    { SrNo: 22, Designation: "Auto Part Backend Executive" },
+    { SrNo: 23, Designation: "Auto Part Sales Executive" },
+    { SrNo: 24, Designation: "Ticketing Executive" },
+    { SrNo: 25, Designation: "Spanish Travel Sales Executive" },
+    { SrNo: 26, Designation: "Travel Sales Executive" },
+    { SrNo: 27, Designation: "IT Executive" },
+    { SrNo: 28, Designation: "CS Executive" },
+    { SrNo: 29, Designation: "Content Writer" },
+    { SrNo: 30, Designation: "Full Stack Developer" },
+    { SrNo: 31, Designation: "Data Analyst" },
+    { SrNo: 32, Designation: "Creative" },
+    { SrNo: 33, Designation: "DevOps" },
+    { SrNo: 34, Designation: "Solution Architect" },
+    { SrNo: 35, Designation: "UI Designer" },
+    { SrNo: 36, Designation: "Frontend Developer" },
+    { SrNo: 37, Designation: "DevOps" },
+    { SrNo: 38, Designation: "UI Designer" },
+    { SrNo: 39, Designation: "Tester" },
+    { SrNo: 40, Designation: "Frontend Developer" },
+    { SrNo: 41, Designation: "Backend Developer" },
+    { SrNo: 42, Designation: "SEO" },
+    { SrNo: 43, Designation: "Support Staff" }
+  ];
 
+  teamMaster = [
+    { id: 1, team: "Airline Sales" },
+    { id: 2, team: "Travoport Sales" },
+    { id: 3, team: "SCF Sales" },
+    { id: 4, team: "BTF Sales" },
+    { id: 5, team: "Fareporto Sales" },
+    { id: 6, team: "BMAT Sales" },
+    { id: 7, team: "Farebuddies Sales" },
+    { id: 8, team: "Business Development" },
+    { id: 9, team: "Quality Team" },
+    { id: 10, team: "Backend/Ticketing" },
+    { id: 11, team: "Work Force Management" },
+    { id: 12, team: "Customer Service Team" },
+    { id: 13, team: "Human Resources Department" },
+    { id: 14, team: "Technology Team" },
+    { id: 15, team: "Support Staff" },
+    { id: 16, team: "Administration" }
+  ];
+
+  get_designation = '';
+  get_team = '';
 
   constructor(public dialogRef: MatDialogRef<EditDialogComponent>,
     @Inject(MAT_DIALOG_DATA) public data: any, private commonService: CommonService) { }
 
+  Designation(val: any) {
+    this.get_designation = val.triggerValue;
+  }
+
+  Team(val: any) {
+    this.get_team = val.triggerValue;
+  }
 
   onClose() {
     this.commonService.showPopup = false;
@@ -32,7 +105,7 @@ export class EditDialogComponent {
   }
 
   onSave() {
+    this.data.Emp_Designation = this.get_designation;
     this.dialogRef.close(this.data);
   }
-
 }
